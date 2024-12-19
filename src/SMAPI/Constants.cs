f492d173110373d01d9b6592592f349074eb19c9
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2024-12-18 22:20:30 -0500

    prepare for release

--- src/SMAPI/Constants.cs
+++ src/SMAPI/Constants.cs
@@ -49,7 +49,7 @@ internal static class EarlyConstants
     internal static int? LogScreenId { get; set; }
 
     /// <summary>SMAPI's current raw semantic version.</summary>
-    internal static string RawApiVersion = "4.1.9";
+    internal static string RawApiVersion = "4.1.10";
 }
 
 /// <summary>Contains SMAPI's constants and assumptions.</summary>
